83e00b0d942c41f6de66253eae749dca585e965d
sa_nk133 <Sunita@277>
2022-03-23 20:04:57 -0400

    all changes done

--- main.py
+++ main.py
@@ -27,7 +27,14 @@ df = df[df['smoking_status'] != 'Unknown']
 x = df['smoking_status'].value_counts()
 x = x.reset_index()
 print(x)
-print(df.count())
+
+#filtering data and removing data with other in gender value types in dataframe
+df = df[df['gender'] != 'Other']
+x = df['gender'].value_counts()
+x = x.reset_index()
+print(x)
+
+
 
 #divinding into groups with stroke and nostroke as 0 and 1
 print(df.groupby('stroke').size())
@@ -91,11 +98,28 @@ print(df_test)
 df_test=df[df['stroke']==1].groupby(['Residence_type','stroke']).size().reset_index(name='count')
 print(df_test)
 
+#converting gender as 0 and 1
+from sklearn import preprocessing
+label = preprocessing.LabelEncoder()
+df['gender'] = label.fit_transform(df['gender'])
+print(df)
+
+str_data = df.select_dtypes(include=['object'])
+str_dt = df.select_dtypes(include=['object'])
+int_data = df.select_dtypes(include = ['integer','float'])
+int_dt = df.select_dtypes(include = ['integer','float'])
+label = LabelEncoder()
+df = str_data.apply(label.fit_transform)
+df= df.join(int_data)
+print(df.head())
+
 #lets build machine learning model
-X = df[['age', 'hypertension', 'heart_disease','avg_glucose_level']]
+X = df[['age', 'hypertension', 'heart_disease','avg_glucose_level','gender','work_type','ever_married','Residence_type','smoking_status','bmi']]
 y = df['stroke']
-print(X)
-print(y)
+
+
+
+
 
 from sklearn.model_selection import train_test_split
 #random_state: set seed for random# generator
@@ -113,7 +137,9 @@ print("lr.intercept_: {}".format(lr.intercept_))
 print("Training set score: {:.2f}".format(lr.score(X_train, y_train)))
 print("Test set score: {:.2f}".format(lr.score(X_test, y_test)))
 
+
+
 # first example: a small fruit with mass 15g, color_score = 5.5, width 4.3 cm, height 5.5 cm
-testFruit = pd.DataFrame([[101, 1, 1, 202.21]], columns=['age', 'hypertension', 'heart_disease','avg_glucose_level'])
+testFruit = pd.DataFrame([[101, 1, 1, 202.21,1,2,1,2,1,36.6]], columns=['age', 'hypertension', 'heart_disease','avg_glucose_level','gender','work_type','ever_married','Residence_type','smoking_status','bmi'])
 fruit_prediction = lr.predict(testFruit)
 print(fruit_prediction)
